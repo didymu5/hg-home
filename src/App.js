1259cce6b796fad163aee62f62d548e6d82cfc58
Thomas X. Wu <thomas.wu@kp.org>
2020-03-18 13:13:03 -0700

    latest batch of tests

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Example from './Example'
+import useFetch from 'use-http'
 import './App.css'
+const url = window.location.href + 'test.json'
 function App() {
-  return <Example />
+  const [greeting, setGreeting] = useState()
+  const { data, loading, error } = useFetch(url, {data: 'hi'})
+  const [request, response] = useFetch(url)
+  useEffect( () => {
+    getData()
+  }, [])
+  const getData = async () => {
+    const data = await request.get()
+    console.log(data)
+    setGreeting(data)
+  }
+  return (
+    <div>
+    {loading && 'Loading...'}
+    {JSON.stringify(greeting)}
+    <p>{JSON.stringify(data)} {data}</p>
+    {JSON.stringify(error)}
+    <hr />
+    </div>
+  )
 }
 
 export default App
